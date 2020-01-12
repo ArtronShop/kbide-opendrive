18f041783f8bd6592063e23ce8a012baf2190ee6
Max <max30012540@hotmail.com>
2020-01-13 00:32:37 +0700

    Upload

---
+++ blocks/block_OpenDrive.js
@@ -0,0 +1,93 @@
+Blockly.Blocks['opendrive_setup'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("OpenDrive setup at 1: (A :")
+            .appendField(new Blockly.FieldDropdown([["OUT1","26"], ["OUT2","27"], ["DAC1","25"], ["18","18"], ["19","19"], ["23","23"]]), "IN1A")
+            .appendField(", B :")
+            .appendField(new Blockly.FieldDropdown([["OUT1","26"], ["OUT2","27"], ["DAC1","25"], ["18","18"], ["19","19"], ["23","23"]]), "IN1B")
+            .appendField("), 2 : (A :")
+            .appendField(new Blockly.FieldDropdown([["OUT1","26"], ["OUT2","27"], ["DAC1","25"], ["18","18"], ["19","19"], ["23","23"]]), "IN2A")
+            .appendField(", B :")
+            .appendField(new Blockly.FieldDropdown([["OUT1","26"], ["OUT2","27"], ["DAC1","25"], ["18","18"], ["19","19"], ["23","23"]]), "IN2B")
+            .appendField(")");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['opendrive_move_forward'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("move forward at power")
+            .appendField(new Blockly.FieldNumber(50, 0, 100, 1), "power")
+            .appendField("% for ")
+            .appendField(new Blockly.FieldNumber(1, 0), "time")
+            .appendField("secs");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['opendrive_move_backward'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("move forward at power")
+            .appendField(new Blockly.FieldNumber(50, 0, 100, 1), "power")
+            .appendField("% for ")
+            .appendField(new Blockly.FieldNumber(1, 0), "time")
+            .appendField("secs");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['opendrive_move'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["move forward","1"], ["move backward","2"], ["turn left","3"], ["turn right","4"]]), "move")
+            .appendField("at power")
+            .appendField(new Blockly.FieldNumber(50, 0, 100, 1), "power")
+            .appendField("%");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+  };
+
+Blockly.Blocks['opendrive_move_wheel'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("left wheel turns at power")
+            .appendField(new Blockly.FieldNumber(50, -100, 100, 1), "power1")
+            .appendField("%, right wheel turns at power")
+            .appendField(new Blockly.FieldNumber(50, -100, 100, 1), "power2");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+  };
+
+Blockly.Blocks['opendrive_stop'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("stop moving");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+  };
---
+++ blocks/generator_OpenDrive.js
@@ -0,0 +1,128 @@
+var ledc_ch = [ 10, 11, 12, 13 ];
+
+Blockly.JavaScript['opendrive_setup'] = function(block) {
+    var dropdown_in1a = block.getFieldValue('IN1A');
+    var dropdown_in1b = block.getFieldValue('IN1B');
+    var dropdown_in2a = block.getFieldValue('IN2A');
+    var dropdown_in2b = block.getFieldValue('IN2B');
+    var code = `
+    ledcSetup(${ledc_ch[0]}, 500, 10);
+    ledcAttachPin(${dropdown_in1a}, ${ledc_ch[0]});
+    ledcSetup(${ledc_ch[1]}, 500, 10);
+    ledcAttachPin(${dropdown_in1b}, ${ledc_ch[1]});
+    ledcSetup(${ledc_ch[2]}, 500, 10);
+    ledcAttachPin(${dropdown_in2a}, ${ledc_ch[2]});
+    ledcSetup(${ledc_ch[3]}, 500, 10);
+    ledcAttachPin(${dropdown_in2b}, ${ledc_ch[3]});
+    `;
+    return code;
+};
+
+Blockly.JavaScript['opendrive_move_forward'] = function(block) {
+    var number_power = block.getFieldValue('power');
+    var number_time = block.getFieldValue('time');
+    var code = `
+    ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[1]}, 1023);
+    ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[3]}, 1023);
+    delay(${number_time} * 1000.0);
+    ledcWrite(${ledc_ch[0]}, 1023);
+    ledcWrite(${ledc_ch[1]}, 1023);
+    ledcWrite(${ledc_ch[2]}, 1023);
+    ledcWrite(${ledc_ch[3]}, 1023);
+    `;
+    return code;
+};
+
+Blockly.JavaScript['opendrive_move_backward'] = function(block) {
+    var number_power = block.getFieldValue('power');
+    var number_time = block.getFieldValue('time');
+    var code = `
+    ledcWrite(${ledc_ch[0]}, 1023);
+    ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[2]}, 1023);
+    ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+    delay(${number_time} * 1000.0);
+    ledcWrite(${ledc_ch[0]}, 1023);
+    ledcWrite(${ledc_ch[1]}, 1023);
+    ledcWrite(${ledc_ch[2]}, 1023);
+    ledcWrite(${ledc_ch[3]}, 1023);
+    `;
+    return code;
+};
+
+Blockly.JavaScript['opendrive_move'] = function(block) {
+    var dropdown_move = block.getFieldValue('move');
+    var number_power = block.getFieldValue('power');
+    var code = '';
+    if (dropdown_move == 1) { // forward
+        code = `
+        ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[1]}, 1023);
+        ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[3]}, 1023);
+        `;
+    } else if (dropdown_move == 2) { // backward
+        code = `
+        ledcWrite(${ledc_ch[0]}, 1023);
+        ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[2]}, 1023);
+        ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+        `;
+    } else if (dropdown_move == 3) { // turn left
+        code = `
+        ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[1]}, 1023);
+        ledcWrite(${ledc_ch[2]}, 1023);
+        ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+        `;
+    } else if (dropdown_move == 4) { // turn right
+        code = `
+        ledcWrite(${ledc_ch[0]}, 1023);
+        ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[3]}, 1023);
+        `;
+    }
+    return code; 
+};
+
+Blockly.JavaScript['opendrive_move_wheel'] = function(block) {
+    var number_power1 = block.getFieldValue('power1');
+    var number_power2 = block.getFieldValue('power2');
+    var code = '';
+    if (number_power1 > 0) {
+        code += `
+        ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[1]}, 1023);
+        `;
+    } else {
+        code += `
+        ledcWrite(${ledc_ch[0]}, 1023);
+        ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+        `;
+    }
+    if (number_power2 > 0) {
+        code += `
+        ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[3]}, 1023);
+        `;
+    } else {
+        code += `
+        ledcWrite(${ledc_ch[2]}, 1023);
+        ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+        `;
+    }
+    return code;
+};
+
+Blockly.JavaScript['opendrive_stop'] = function(block) {
+    var code = `
+    ledcWrite(${ledc_ch[0]}, 1023);
+    ledcWrite(${ledc_ch[1]}, 1023);
+    ledcWrite(${ledc_ch[2]}, 1023);
+    ledcWrite(${ledc_ch[3]}, 1023);
+    `;
+    return code;
+};
