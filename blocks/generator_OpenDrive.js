0cd2b2b6cd0e874abc7124384ce463309eeed43b
Max <max30012540@hotmail.com>
2020-01-16 20:39:56 +0700

    Update generator_OpenDrive.js

--- blocks/generator_OpenDrive.js
+++ blocks/generator_OpenDrive.js
@@ -22,9 +22,9 @@ Blockly.JavaScript['opendrive_move_forward'] = function(block) {
     var number_power = block.getFieldValue('power');
     var number_time = block.getFieldValue('time');
     var code = `
-    ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[0]}, (100 - ${number_power}) / 100.0 * 1023.0);
     ledcWrite(${ledc_ch[1]}, 1023);
-    ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[2]}, (100 - ${number_power}) / 100.0 * 1023.0);
     ledcWrite(${ledc_ch[3]}, 1023);
     delay(${number_time} * 1000.0);
     ledcWrite(${ledc_ch[0]}, 1023);
@@ -40,9 +40,9 @@ Blockly.JavaScript['opendrive_move_backward'] = function(block) {
     var number_time = block.getFieldValue('time');
     var code = `
     ledcWrite(${ledc_ch[0]}, 1023);
-    ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[1]}, (100 - ${number_power}) / 100.0 * 1023.0);
     ledcWrite(${ledc_ch[2]}, 1023);
-    ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+    ledcWrite(${ledc_ch[3]}, (100 - ${number_power}) / 100.0 * 1023.0);
     delay(${number_time} * 1000.0);
     ledcWrite(${ledc_ch[0]}, 1023);
     ledcWrite(${ledc_ch[1]}, 1023);
@@ -58,30 +58,30 @@ Blockly.JavaScript['opendrive_move'] = function(block) {
     var code = '';
     if (dropdown_move == 1) { // forward
         code = `
-        ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[0]}, (100 - ${number_power}) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[1]}, 1023);
-        ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[2]}, (100 - ${number_power}) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[3]}, 1023);
         `;
     } else if (dropdown_move == 2) { // backward
         code = `
         ledcWrite(${ledc_ch[0]}, 1023);
-        ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[1]}, (100 - ${number_power}) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[2]}, 1023);
-        ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[3]}, (100 - ${number_power}) / 100.0 * 1023.0);
         `;
     } else if (dropdown_move == 3) { // turn left
         code = `
-        ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[0]}, (100 - ${number_power}) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[1]}, 1023);
         ledcWrite(${ledc_ch[2]}, 1023);
-        ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[3]}, (100 - ${number_power}) / 100.0 * 1023.0);
         `;
     } else if (dropdown_move == 4) { // turn right
         code = `
         ledcWrite(${ledc_ch[0]}, 1023);
-        ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
-        ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[1]}, (100 - ${number_power}) / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[2]}, (100 - ${number_power}) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[3]}, 1023);
         `;
     }
@@ -94,24 +94,24 @@ Blockly.JavaScript['opendrive_move_wheel'] = function(block) {
     var code = '';
     if (number_power1 > 0) {
         code += `
-        ledcWrite(${ledc_ch[0]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[0]}, (100 - abs(${number_power})) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[1]}, 1023);
         `;
     } else {
         code += `
         ledcWrite(${ledc_ch[0]}, 1023);
-        ledcWrite(${ledc_ch[1]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[1]}, (100 - abs(${number_power})) / 100.0 * 1023.0);
         `;
     }
     if (number_power2 > 0) {
         code += `
-        ledcWrite(${ledc_ch[2]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[2]}, (100 - abs(${number_power})) / 100.0 * 1023.0);
         ledcWrite(${ledc_ch[3]}, 1023);
         `;
     } else {
         code += `
         ledcWrite(${ledc_ch[2]}, 1023);
-        ledcWrite(${ledc_ch[3]}, ${number_power} / 100.0 * 1023.0);
+        ledcWrite(${ledc_ch[3]}, (100 - abs(${number_power})) / 100.0 * 1023.0);
         `;
     }
     return code;
